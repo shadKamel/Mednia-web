b670ce617eef51dcfc9daf18c5bccd551824d36a
shad kamil <kamel.shad.sk@gmail.com>
2024-01-13 22:57:46 +0300

    test

--- app.vue
+++ app.vue
@@ -1,17 +1,17 @@
 <template>
-    <div>
-        <NuxtLoadingIndicator color="amber" />
-        <NuxtPageLoader v-if="loading" />
-        <NuxtLayout v-else-if="!loading">
-            <NuxtPage />
-        </NuxtLayout>
-    </div>
+  <div>
+    <NuxtLoadingIndicator color="amber" />
+    <NuxtPageLoader v-if="loading" />
+    <NuxtLayout v-else-if="!loading">
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
 
 <script lang="ts" setup>
-const nuxt = useNuxtApp()
-const loading = ref(true)
-nuxt.hook('app:mounted', (app) => {
-    loading.value = false
-})
+const nuxt = useNuxtApp();
+const loading = ref(true);
+nuxt.hook("app:mounted", (app) => {
+  loading.value = false;
+});
 </script>
